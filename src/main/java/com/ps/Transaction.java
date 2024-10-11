5920396e67c596a20233cb7691ca44a63d5fc7b2
Christian <you@example.com>
2024-10-11 14:37:37 -0500

    added transaction class

---
+++ src/main/java/com/ps/Transaction.java
@@ -0,0 +1,72 @@
+package com.ps;
+// Creating a transaction class so we can call it in the Main class.
+public class Transaction {
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transaction(String date, String time,String description,String vendor,double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+
+
+
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "amount=" + amount +
+                ", date='" + date + '\'' +
+                ", time='" + time + '\'' +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                '}';
+    }
+}
+
