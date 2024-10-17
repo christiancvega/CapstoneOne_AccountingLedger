402b0a2d8538397618fb32214e80adcc06e5e8cd
Christian <you@example.com>
2024-10-17 12:45:20 -0500

    subSubmenu functional

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -205,19 +205,18 @@ public class Main {
         }
         System.out.println("---------------------------------------");
     }
-// create a header for the display list
+
+    // create a header for the display list
     public static void displayDeposits() {
         System.out.println("All Deposits");
         System.out.println("Time, Date, Description, Vendor, Amount");
         System.out.println("---------------------------------------");
-// check for deposits
-        boolean hasDeposits = false;
+
 // go through array list and check for deposits
         for (Transaction transaction : transactions) {
             // search for positive transactions and print
-            if (transaction.getAmount() > 0){
+            if (transaction.getAmount() > 0) {
                 System.out.println(transaction);
-                hasDeposits = true;
             }
         }
         System.out.println("---------------------------------------");
@@ -228,14 +227,12 @@ public class Main {
         System.out.println("All Payments: ");
         System.out.println("Time. Date, Description, Vendor, Amount");
         System.out.println("---------------------------------------");
-        // check for deposits
-        boolean hasPayments = false;
+
         // go through array list for desposits
-        for(Transaction transaction : transactions) {
+        for (Transaction transaction : transactions) {
             // if transaction is negative print
-            if (transaction.getAmount()< 0) {
+            if (transaction.getAmount() < 0) {
                 System.out.println(transaction);
-                hasPayments = true;
             }
         }
         System.out.println("---------------------------------------");
@@ -285,22 +282,101 @@ public class Main {
 
     // methods to connect to my cases
     public static void displayMonthToDate() {
-        System.out.println("Display month to date");
+        //Get the current date first and get first day of the month
+        LocalDate currentDate = LocalDate.now();
+        LocalDate firstDayOfMonth = currentDate.withDayOfMonth(1);
+
+        System.out.println("This month's transactions: ");
+        System.out.println("Time, Date, Description, Vendor, Amount");
+        System.out.println("---------------------------------------");
+
+
+
+        for (Transaction transaction : transactions) {
+            if ((transaction.getDate().isAfter(firstDayOfMonth.minusDays(1)) || transaction.getDate().isEqual(firstDayOfMonth))
+                    && transaction.getDate().isBefore(currentDate.plusDays(1))) {
+                System.out.println(transaction);
+
+            }
+
+
+        }
+        System.out.println("---------------------------------------");
+
     }
 
     public static void displayPreviousMonth() {
-        System.out.println("Display previous month");
+        LocalDate currentDate = LocalDate.now();
+        LocalDate firstDayOfPreviousMonth = currentDate.minusMonths(1).withDayOfMonth(1);
+        LocalDate lastDayOfPreviousMonth = currentDate.minusMonths(1).withDayOfMonth(currentDate.minusMonths(1).lengthOfMonth());
+
+        System.out.println("Transactions for the previous month:");
+        System.out.println("Time, Date, Description, Vendor, Amount");
+        System.out.println("-----------------------------------------------------------");
+
+
+
+        for (Transaction transaction : transactions) {
+            if ((transaction.getDate().isAfter(firstDayOfPreviousMonth.minusDays(1)) || transaction.getDate().isEqual(firstDayOfPreviousMonth))
+                    && (transaction.getDate().isBefore(lastDayOfPreviousMonth.plusDays(1)))) {
+                System.out.println(transaction);
+
+            }
+        }
+
+        System.out.println("-----------------------------------------------------------");
     }
 
     public static void displayYearToDate() {
-        System.out.println("Dislay year to date");
+        LocalDate currentDate = LocalDate.now();
+        LocalDate firstDayOfYear = currentDate.withDayOfYear(1);
+
+        System.out.println("Transactions for the current year:");
+        System.out.println("Time, Date, Description, Vendor, Amount");
+        System.out.println("-----------------------------------------------------------");
+
+
+
+        for (Transaction transaction : transactions) {
+            if (transaction.getDate().isAfter(firstDayOfYear.minusDays(1)) && transaction.getDate().isBefore(currentDate.plusDays(1))) {
+                System.out.println(transaction);
+
+            }
+        }
+        System.out.println("-----------------------------------------------------------");
     }
 
     public static void displayPreviousYear() {
-        System.out.println("Display previous year");
-    }
+        LocalDate currentDate = LocalDate.now();
+        LocalDate firstDayOfPreviousYear = currentDate.minusYears(1).withDayOfYear(1);
+        LocalDate lastDayOfPreviousYear = currentDate.minusYears(1).withDayOfYear(currentDate.minusYears(1).lengthOfYear());
 
+        System.out.println("Transactions for the previous year:");
+        System.out.println("Time, Date, Description, Vendor, Amount");
+        System.out.println("-----------------------------------------------------------");
+
+
+        for (Transaction transaction : transactions) {
+            if (transaction.getDate().isAfter(firstDayOfPreviousYear.minusDays(1)) && transaction.getDate().isBefore(lastDayOfPreviousYear.plusDays(1))) {
+                System.out.println(transaction);
+            }
+        }
+        System.out.println("-----------------------------------------------------------");
+    }
     public static void searchByVendor() {
-        System.out.println("Search by vendor");
+        commandScanner.nextLine();
+        System.out.println("Enter the vendor name:");
+        String vendorName = inputScanner.nextLine();
+
+        System.out.println("Transactions for vendor: " + vendorName);
+        System.out.println("Time, Date, Description, Vendor, Amount");
+        System.out.println("-----------------------------------------------------------");
+
+        for (Transaction transaction : transactions) {
+            if (transaction.getVendor().equalsIgnoreCase(vendorName)) {
+                System.out.println(transaction);
+            }
+        }
+        System.out.println("-----------------------------------------------------------");
     }
 }
